3b7bd2950ad06c72e55a73d8a48fcef70f7d0743
juampa95 <jpmanzano95@gmail.com>
2023-09-12 23:26:33 -0300

    agregado de modelo unidosis

---
+++ app/models_unidosis.py
@@ -0,0 +1,41 @@
+from sqlmodel import Field, SQLModel, Relationship
+from datetime import date, datetime
+from typing import List, Optional
+from pydantic import validator
+from app.models import Medicine
+
+
+class Base2(SQLModel):
+    id: Optional[int] = Field(primary_key=True, index=True, description="Código id creado automáticamente")
+
+
+class BaseModel2(Base2):
+    created_at: datetime = Field(default_factory=datetime.now)
+    update_at: datetime = Field(default_factory=datetime.now)
+
+    def on_update(self):
+        self.update_at = datetime.now()
+
+
+class UnidosisMedicine(BaseModel2, table=True):
+    visual_config1: Optional[str] = Field(default="")
+    visual_config2: Optional[str] = Field(default="")
+    visual_config3: Optional[str] = Field(default="")
+    visual_config4: Optional[str] = Field(default="")
+    adv1: Optional[bool] = Field(default=False)
+    adv2: Optional[bool] = Field(default=False)
+    adv3: Optional[bool] = Field(default=False)
+    adv4: Optional[bool] = Field(default=False)
+    adv5: Optional[bool] = Field(default=False)
+    tac: Optional[bool] = Field(default=False)
+    pregnancy_cat: Optional[str]
+
+    medicine_id: int = Field(foreign_key=Medicine.id, nullable=False)
+
+
+    @validator("pregnancy_cat", pre=True, always=False)
+    def validate_pregnacy_cat(cls, value):
+        if len(value) > 4:
+            raise ValueError("La categoria de embarazo puede tener un maximo de 3 caracteres")
+        value = str.capitalize(value)
+        return value
---
+++ app/routers/router_unidosis.py
@@ -0,0 +1,58 @@
+from fastapi import APIRouter, HTTPException, Depends
+from typing import List
+from sqlmodel import Session, select
+from app import models_unidosis
+from app.models_unidosis import UnidosisMedicine as model
+from app.db import get_session
+from app.models import Medicine
+
+router = APIRouter(prefix="/unidosis")
+
+@router.get("/list")
+async def get_unidosis_list(session: Session = Depends(get_session)):
+    try:
+        statement = select(model)
+        objet = session.exec(statement).all()
+        return objet
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        raise e  # Esto volverá a lanzar la excepción para obtener más información detallada en la respuesta
+
+
+
+@router.get("/{object_id}")
+async def get_unidosis_details(object_id: int, session: Session = Depends(get_session)):
+    try:
+        unidosis = session.query(model).filter_by(id=object_id).first()
+        if not unidosis:
+            raise HTTPException(status_code=404, detail="Unidosis not found")
+
+        medicine = session.query(Medicine).filter_by(id=unidosis.medicine_id).first()
+
+        medicine_info = {
+            "code": medicine.code,
+            "name": medicine.name,
+            "drug": medicine.drug,
+            "concentration": medicine.concentration
+        }
+
+        response = {
+            "unidosis" : unidosis,
+            "medicine": medicine_info
+        }
+    except:
+        raise HTTPException(status_code=404, detail="unidosis details not found")
+    # statement = select(model).where(model.id == object_id)
+    # object = session.exec(statement).first()
+    # if object is None:
+    #     raise HTTPException(status_code=404, detail="unidosis details not found")
+    # return object
+    return response
+
+@router.post("/create")
+async def create_unidosis_model(object: model, session: Session = Depends(get_session)):
+    session.add(object)
+    session.commit()
+    session.refresh(object)
+    return object
+
